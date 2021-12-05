d9341d19b6205c0fbec0904fce1e20473a8140ab
starking331 <79844497+starking331@users.noreply.github.com>
2021-12-05 14:24:14 +0400

    Debug

--- Assets/Scripts/FirstPlayer.cs
+++ Assets/Scripts/FirstPlayer.cs
@@ -5,11 +5,17 @@ using UnityEngine.UI;
 
 public class FirstPlayer : MonoBehaviour
 {
-    [SerializeField] int money;
+    [SerializeField] int moneyFirst;
+    [SerializeField] int moneySecond;
     // Start is called before the first frame update
     public void ButtonClick()
     {
-        money++;
+        moneyFirst++;
+    }
+
+    public void ButtonClickSecondPlayer()
+    {
+        moneySecond++;
     }
 
     // Update is called once per frame
