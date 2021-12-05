5fc27e3897f003be64943894537084ebd5e8847a
evgeniyleet <evgeniy.leet1337@gmail.com>
2021-12-05 15:14:09 +0400

    добавлен временной отсчет

--- Assets/Scripts/BothPlayer.cs
+++ Assets/Scripts/BothPlayer.cs
@@ -7,6 +7,8 @@ public class BothPlayer : MonoBehaviour
 {
     [SerializeField] int scoreFirst;
     [SerializeField] int scoreSecond;
+    [SerializeField] double coinFirst;
+    [SerializeField] double coinSecond;
 
     public void ButtonClickFirstPlayer()
     {
@@ -17,9 +19,32 @@ public class BothPlayer : MonoBehaviour
     {
         scoreSecond++;
     }
-
+    void Start()
+    {
+        InvokeRepeating("TimeStartGame", 2.0f, 3f);
+    }
     void Update()
     {
         
     }
+    public void TimeStartGame()
+    {
+        if((scoreFirst - scoreSecond) >= 10)
+        {
+            coinFirst += 0.1;
+        }
+        if((scoreSecond - scoreFirst) >= 10)
+        {
+            coinSecond += 0.1;
+        }
+        if((scoreFirst - scoreSecond) >= 1000)
+        {
+            //FIRST PLAYER WIN
+        }
+        if((scoreSecond - scoreFirst) >= 1000)
+        {
+            //SECOND PLAYER WIN
+        }
+    }
+
 }
