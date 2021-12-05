313247c197efbc4329eb8a50fd71dd95c797121f
starking331 <79844497+starking331@users.noreply.github.com>
2021-12-05 15:51:48 +0400

    FixedAnim

--- Assets/Scripts/CreateWeapon.cs
+++ Assets/Scripts/CreateWeapon.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class CreateWeapon : MonoBehaviour
 {
+
     public void CreateWeaponFirstPlayer(GameObject objPrefab)
     {
-        Instantiate(objPrefab, objPrefab.transform.position, objPrefab.transform.rotation);
+        var weapon = Instantiate(objPrefab, objPrefab.transform.position, objPrefab.transform.rotation);
+        weapon.GetComponent<Animator>().SetTrigger("trig");
     }
 
     public void CreateWeaponSecondPlayer(GameObject objPrefab)
     {
-        Instantiate(objPrefab, objPrefab.transform.position, objPrefab.transform.rotation);
+        var weapon = Instantiate(objPrefab, objPrefab.transform.position, objPrefab.transform.rotation);
+        weapon.GetComponent<Animator>().SetTrigger("trig");
     }
 }
